b97cfd17ad75319dd45f66360f2f32ae7ea4e8f9
Rubens Mititaka Kishimoto <rubenskishimoto@gmail.com>
2024-10-29 12:24:38 -0300

    feat: generate store links dinamically :sparkles:

--- content.js
+++ content.js
@@ -1,5 +1,13 @@
 const leftSideInventoryContainer = document.querySelectorAll('.inventory_page_left');
 
+const allStores = [
+    {
+        "id": 1,
+        "href": "https://skin.land/pt/market/csgo/?query=",
+        "store_name":"Skin.land",
+    }
+]
+
 let button = document.createElement('button');
     button.textContent = 'Price on other stores';
     button.className = 'customButton';
@@ -8,10 +16,41 @@ let linkContainer = document.createElement('div');
     linkContainer.className = 'linkContainer';
     linkContainer.style.display = 'none';
 
-let link1 = document.createElement('a');
-    link1.href = `https://skin.land/pt/market/csgo/?query=`;
-    link1.textContent = 'Skin.land';
-    link1.target = '_blank';
+const isPageRightVisible = document.querySelector('.inventory_page_right');
+let firstLoad = true;
+
+if(isPageRightVisible && firstLoad){
+    const innerDivs = isPageRightVisible.querySelectorAll('.inventory_iteminfo'); 
+    const itemWithZIndexOne = innerDivs[innerDivs.length - 1]
+    const itemTitle1 = document.querySelector('#iteminfo1_item_name');
+   
+    const observer = new MutationObserver((mutationsList) => {
+        mutationsList.forEach((mutation) => {
+          if (mutation.type === 'childList') {
+            if (itemTitle1.textContent.length > 0) {
+                linkContainer.innerHTML = '';
+
+                allStores.forEach(store => {
+                    let link = document.createElement('a');
+                        link.href = `${store.href}${itemTitle1.textContent}`;
+                        link.textContent = `${store.store_name}`;
+                        link.target = '_blank';
+                        link.className = 'storeLink';
+                        link.id = store.id;
+            
+                        linkContainer.appendChild(link);
+                });
+
+                firstLoad = false;
+            }
+
+            itemWithZIndexOne.appendChild(button);
+            itemWithZIndexOne.appendChild(linkContainer);
+
+        }});
+      });
+    observer.observe(itemTitle1, { childList: true, subtree: true }); 
+}
 
 leftSideInventoryContainer.forEach(item => {
   item.addEventListener('click', () => {
@@ -23,24 +62,30 @@ leftSideInventoryContainer.forEach(item => {
     });
 
     const innerItemName = itemWithZIndexOne.querySelector('.item_desc_content.app730.context2 .item_desc_description .hover_item_name');
-    const buttonExist = itemWithZIndexOne.querySelector('.customButton');
+    const buttonExist = document.querySelector('.customButton');
+    const allLinks = document.querySelectorAll('.storeLink');
 
     if(buttonExist){
-        link1.href = `https://skin.land/pt/market/csgo/?query=${innerItemName.textContent}`;
-    }else{
-        link1.href = `https://skin.land/pt/market/csgo/?query=${innerItemName.textContent}`;
-        linkContainer.appendChild(link1);
-        itemWithZIndexOne.appendChild(button);
-        itemWithZIndexOne.appendChild(linkContainer);
-    }    
+        allLinks.forEach(link => {
+            allStores.forEach(store => {
+                if(link.id == store.id){
+                    link.href = ``;
+                    link.href = `${store.href}${innerItemName.textContent}`;
+                }
+            });   
+        });
+    } 
+
+    itemWithZIndexOne.appendChild(button);
+    itemWithZIndexOne.appendChild(linkContainer);
   });
+});
 
 
-  button.addEventListener('click', () => {
+button.addEventListener('click', () => {
     if (linkContainer.style.display === 'none') {
         linkContainer.style.display = 'block';
     } else {
         linkContainer.style.display = 'none';
     }      
-});
 });
